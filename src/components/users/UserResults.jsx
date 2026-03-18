45e36f70c1e9a5b806a98646b5340a2e59879072
Matheus Silva <msilvaqs@outlook.com>
2026-03-18 10:38:48 -0400

    Loading github users

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+
+const UserResults = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
+        headers: {
+          authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
+        },
+      });
+
+      const data = await response.json();
+
+      setUsers(data);
+      setLoading(false);
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+};
+
+export default UserResults;
